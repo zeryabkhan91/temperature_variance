5c0b3c0cc5e991dd177736a69591d823e4cedd14
Zeryab Khan <zeryabkhan91@gmail.com>
2022-11-30 19:26:59 +0500

    Add seeds file to create DataSet for 10 minutes

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+  DataSet.delete_all
+  numbers = [*-100..100]
+  (DateTime.now.to_i..10.minutes.from_now.to_i).step(1.second).each do |time|
+    puts "Creating DataSet at time"
+    puts Time.at(time)
+    DataSet.create!(temperature: numbers.sample, recorded_at: Time.at(time))
+  end
+
